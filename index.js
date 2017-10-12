3455fe5b84098910d371843e188c86c9d843f055
sumomori <qe2277@gmail.com>
2017-10-12 10:00:21 +0000

    １７の倍数か判定モジュール

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
 
-module.exports = {
+/**
+ *17の倍数である場合、真を返す
+ * @param {number} 
+ */
+
+function isMultipleOfSeventeen(numbers){
+    return numbers % 17 === 0;
 }
+
+module.exports = {
+isMultipleOfSeventeen: isMultipleOfSeventeen
+};
